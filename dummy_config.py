f7e49a5121832c12684524bb24e9628891e4d259
codekansas <bkbolte18@gmail.com>
2016-03-27 17:21:30 -0400

    added dummy_config.py

---
+++ dummy_config.py
@@ -0,0 +1,22 @@
+''' Dummy configuration file '''
+
+import os
+
+# change to ':memory:' to create the database in memory
+BASE_DATA_PATH = '/media/moloch/HHD/MachineLearning/data/yahoo_qa/'
+
+DB_PATH = os.path.join(BASE_DATA_PATH, 'db.sqlite3')
+
+DATASETS = {
+    'small_sample': os.path.join(BASE_DATA_PATH, 'small_sample.xml'),
+    'full_part_1': os.path.join(BASE_DATA_PATH, 'FullOct2007.xml.part1'),
+    'full_part_2': os.path.join(BASE_DATA_PATH, 'FullOct2007.xml.part2')
+}
+
+# maximum string lengths, mostly for specifying the database model
+STRING_LENGTHS = {
+    'question_title': 200,
+    'question_content': 2000,
+    'answer_content': 2000,
+    'category': 100,
+}
